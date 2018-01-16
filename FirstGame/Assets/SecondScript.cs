6db3004e5c1e83b406fc9e7e8d583eef5850b427
Michael Christensen <iloveicecream1016@gmail.com>
2018-01-16 11:17:56 -0700

    Notes on Second Script

---
+++ FirstGame/Assets/SecondScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class is an object classification
+//you must have open and closed {} per object
+
+public class SecondScript : MonoBehaviour {
+
+//void is a function that is void or empty or null
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
