1981fe55b7362d37067406bdb93f832408c90af4
Michael Christensen <iloveicecream1016@gmail.com>
2018-01-18 11:15:30 -0700

    Removed trash. Made a cube that says stuff.
    I would have had a tiger but it was demo mode so I didn't.

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//this declares what start will do
+	print("The animal has been eaten."); //Uppercase will error the code
+	print("The hippo is now full.");
+	print("Something sad just happened.");
+	print("Were you expecting this to be a good game?");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
