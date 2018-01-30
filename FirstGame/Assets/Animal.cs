e775d34a3d73eaf1518040e210ecdaaeba734f05
Michael Christensen <iloveicecream1016@gmail.com>
2018-01-30 11:06:23 -0700

    If statements work and notes

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -9,9 +9,29 @@ public class Animal : MonoBehaviour {
 	public int FoodCount;
 
 	public Text myUIText;
+
 	void OnMouseDown()
 	{
-		myUIText.text = ("Hello, " + Name + ". It's time to eat " + FoodCount + " donuts!");
+		if (1+2 == 3)
+		{
+			print("3");
+		}
+		// Two Equal signs become a compare operator. A single equal sign is a operator.
+		// To make an if not statement, type if ("stuff" != "Stuff")
+	
+		if(Name == "Dog")
+		{
+			print("Bark!");
+		}
+
+		if(FoodCount > 0)
+		{
+			print("The pet has food.");
+		}
+		if(Name != "Frog")
+		{
+			print(Name + "is not a reptile. Or amused.");
+		}
 	}
 
 
---
+++ FirstGame/Assets/Doors.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour {
+
+public bool Key = false;
+
+void OnMouseDown()
+{
+    if(Key == true)
+    {
+        print("The door was unlocked");
+    }
+}
+
+}
