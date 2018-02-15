eb7b51d7153be392b0e7aff6feeada976d70291e
Michael Christensen <iloveicecream1016@gmail.com>
2018-02-15 11:35:24 -0700

    2/15 Loops, Arrays, Lists

--- FirstGame/Assets/ArraysNotes.cs
+++ FirstGame/Assets/ArraysNotes.cs
@@ -6,5 +6,9 @@ public class ArraysNotes : MonoBehaviour {
 
 	// An Array is like a fence post. They vary in size but contain the same data type.
 	// Arrays are numbered. Starting from 0. (0,1,2,3,4,5,...)
+	// An Array and a list is a collection.
+	// Don't forget the accessor. The "." to access certain codes or files.
+	// Arrays cannot be changable ingame. A list can. The difference.
+	//Lists are CHANGEABLE.
 	
 }
