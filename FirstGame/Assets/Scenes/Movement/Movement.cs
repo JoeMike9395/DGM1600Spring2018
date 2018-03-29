3f2b2163210d46b71d38d059e410d4961bebbd41
Michael Christensen <iloveicecream1016@gmail.com>
2018-03-29 11:16:12 -0600

    Character move 3/29
    movepattern = fast move scriptable. That's a note.

---
+++ FirstGame/Assets/Scenes/Movement/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public MovePattern MovePattern;
+    private CharacterController controller;
+
+    private void Start () {
+        controller = GetComponent<CharacterController> ();
+    }
+
+    void Update () {
+        MovePattern.Move (controller, transform);
+    }
+}
