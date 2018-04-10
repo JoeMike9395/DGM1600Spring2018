b42a43178bf9757e55674b0c0eec5ecd32c51606
Michael Christensen <iloveicecream1016@gmail.com>
2018-04-10 11:26:52 -0600

    Triggers, Organize, Started Timer.

--- FirstGame/Assets/Scenes/Movement/Trigger.cs
+++ FirstGame/Assets/Scenes/Movement/Trigger.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class Trigger : MonoBehaviour {
 
 	public PowerUp PowerUpTransfer;
+	private MovePattern prevMovePattern
 
 	private void OnTriggerEnter(Collider obj)
 	{
+		MovePattern prevMovePattern = obj.GetComponent<Movement>().MovePattern;
+
 		obj.GetComponent<Movement>().MovePattern = PowerUpTransfer.Transfer();
+
+	
+
+		gameObject.SetActive(false);
 	}
+
+	void RestoreMovePattern (MovePattern oldMovePattern)
+	{
+
+	}
+	
 }
