e1d4ef51bcf960bf3eb73c15522eea0d5f6374f0
Michael Christensen <iloveicecream1016@gmail.com>
2018-04-17 11:25:06 -0600

    Fixed Health, Worked on States.

---
+++ FirstGame/Assets/Scenes/Game/Trigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour {
+
+    public PowerUp PowerUpTransfer;
+    public float PowerLevel = 1.0F;
+    public Image HealthBar;
+
+    public float AddHealth(){
+
+        return HealthBar.fillAmount += PowerLevel;
+    }
+
+    private void OnTriggerEnter (Collider obj) {
+
+        obj.GetComponent<Movement> ().MovePattern = PowerUpTransfer.Transfer ();
+        if (HealthBar.fillAmount > 0); 
+        {
+            PowerLevel = AddHealth();
+        }
+
+        gameObject.SetActive (false);
+    }
+
+    public void ColorChange(){
+        if (PowerLevel < 0.5F);
+        {
+            HealthBar.color = Color.green;
+        }
+    }
+
+}
