d7700c8ad4fde7fb881d9ca34258ea359e3b9757
Michael Christensen <iloveicecream1016@gmail.com>
2018-05-03 17:51:21 -0600

    Added Enums and Switch
    This change was made at home with a newer version of Unity. This may result in errors or loss of assets when opened. If so OPEN THE PREVIOUS COMMIT and grade the Switch and Enums off of this commit here.

--- FirstGame/Assets/Scenes/Game/Trigger.cs
+++ FirstGame/Assets/Scenes/Game/Trigger.cs
@@ -11,7 +11,11 @@ public class Trigger : MonoBehaviour {
     public Color Healthy;
     public Color Low;
     public Color Critical;
-
+    public enum Collect
+    {
+        SpeedBoost, AntiGrav, Explosion
+    }
+    public Collect PlayerPower;
     public float AddHealth(){
 
         return HealthBar.fillAmount += PowerLevel;
@@ -37,6 +41,22 @@ public class Trigger : MonoBehaviour {
         else
         {
             HealthBar.color = Color.red;
+        }
+
+        switch(PlayerPower)
+        {
+            case Collect.SpeedBoost:
+                print("You're a very fast boi.");
+                break;
+
+            case Collect.AntiGrav:
+                print("You feel funny.");
+                break;
+
+            case Collect.Explosion:
+                print("Exaplossssion.");
+                break;
+           
         }
 
         while (HealthBar.fillAmount <= 0.1F)
